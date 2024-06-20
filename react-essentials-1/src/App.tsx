c7f1aa27cec2e772ee9beb2c10897d1b6796687e
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-06-20 13:56:04 +0530

    [components]

--- react-essentials-1/src/App.tsx
+++ react-essentials-1/src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import reactImg from "./assets/react-core-concepts.png";
+import componentImg from "./assets/components.png";
+
+type Props = {
+  title: string;
+  description: string;
+  img: string;
+}
+
+function Header () {
+  const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+  function genRandomInt(max: number){
+    return Math.floor(Math.random() * (max + 1));
+  }
+
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return(
+    <header>
+        <img src={reactImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+  );
+}
+
+function CoreConcept(props: Props) {
+  return(
+    <li>
+      <img src="" alt=""></img>
+      <h3></h3>
+      <p></p>
+    </li>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts!</h2>
+          <ul>
+            <CoreConcept title={"Components"} description={""} img={componentImg}/>
+            <CoreConcept title={""} description={""} img={""} />
+            <CoreConcept title={""} description={""} img={""} />
+            <CoreConcept title={""} description={""} img={""} />
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
---
+++ react-essentials-1/src/components/NewComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const NewComponent = (props: Props) => {
+  return (
+    <div>NewComponent</div>
+  )
+}
+
+export default NewComponent
