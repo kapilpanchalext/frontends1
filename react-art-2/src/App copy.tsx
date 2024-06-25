d7c18ec82704391f9ca2dcab1ac74684595322af
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-06-25 10:48:25 +0530

    [react-art-2]

---
+++ react-art-2/src/App copy.tsx
@@ -0,0 +1,15 @@
+import AuthInputs from "./components/authinputs/AuthInputs";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+}
+
+export default App;
