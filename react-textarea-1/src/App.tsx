4ff7aa802a3de58085fbe908281056bedce16e40
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-07-02 14:40:45 +0530

    [react textarea]

--- react-textarea-1/src/App.tsx
+++ react-textarea-1/src/App.tsx
@@ -1,4 +1,7 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useRef } from "react";
+import useFloatingToolbar from "./hooks/useFloatingToolbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAlignCenter, faAlignJustify, faAlignLeft, faAlignRight, faBold, faHeading, faItalic, faParagraph, faUnderline, fas } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
 
@@ -112,55 +115,28 @@ function App() {
 
   const draggableRef = useRef<HTMLDivElement>(null);
   const closeButtonRef = useRef<HTMLButtonElement>(null);
-
-  const makeDraggable = useCallback((element: HTMLElement) => {
-    let offsetX: number, offsetY: number;
-    let isDragging = false;
-
-    const onMouseMove = (event: MouseEvent) => {
-      if (!isDragging) return;
-      element.style.left = `${event.clientX - offsetX}px`;
-      element.style.top = `${event.clientY - offsetY}px`;
-    };
-
-    const onMouseUp = () => {
-      isDragging = false;
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-    };
-
-    element.addEventListener('mousedown', (event) => {
-      isDragging = true;
-      offsetX = event.clientX - element.getBoundingClientRect().left;
-      offsetY = event.clientY - element.getBoundingClientRect().top;
-      document.addEventListener('mousemove', onMouseMove);
-      document.addEventListener('mouseup', onMouseUp);
-    });
-  }, []);
-
-  useEffect(() => {
-    if (draggableRef.current) {
-      makeDraggable(draggableRef.current);
-    }
-
-    if (closeButtonRef.current) {
-      closeButtonRef.current.addEventListener('click', function() {
-        if (draggableRef.current) {
-          draggableRef.current.style.display = 'none';
-        }
-      });
-    }
-  }, [makeDraggable]);
-
+  useFloatingToolbar({ draggableRef, closeButtonRef });
   return (
     <>
       <div
         ref={draggableRef}
-        style={{ width: '100px', height: '100px', backgroundColor: 'lightblue', position: 'absolute' }}
+        className="w-[500px] h-[100px] bg-slate-300 rounded-1xl absolute flex flex-col items-center justify-center"
+        // style={{ width: '500px', height: '100px', backgroundColor: 'lightblue', position: 'absolute' }}
       >
-        Drag me!
+        {/* <h1>Drag me!</h1> */}
+        <div className="flex gap-2">
+          <button><FontAwesomeIcon icon={faAlignLeft} /></button>
+          <button><FontAwesomeIcon icon={faAlignCenter} /></button>
+          <button><FontAwesomeIcon icon={faAlignJustify} /></button>
+          <button><FontAwesomeIcon icon={faAlignRight} /></button>
+          <button><FontAwesomeIcon icon={faBold} /></button>
+          <button><FontAwesomeIcon icon={faItalic} /></button>
+          <button><FontAwesomeIcon icon={faHeading} /></button>
+          <button><FontAwesomeIcon icon={faParagraph} /></button>
+        </div>
+        <button className="bg-slate-600 hover:text-cyan-200" ref={closeButtonRef}>Close</button>
       </div>
-      <button ref={closeButtonRef}>Close</button>
+      
     </>
   )
 }
