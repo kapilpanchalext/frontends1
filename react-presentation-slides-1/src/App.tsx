acde3bbacf9977dfd964f81c8265564b1b30615b
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-07-17 10:49:27 +0530

    [react-presentation-slides]

--- react-presentation-slides-1/src/App.tsx
+++ react-presentation-slides-1/src/App.tsx
@@ -3,7 +3,10 @@ function App() {
 
   return (
     <>
-      <h1>React Presentation Slides</h1>
+      <div style={{ margin: "10px", width: "calc(100% - 20px)", backgroundColor: "gray", height: "95vh", display: "flex", flexDirection: "column", border: "1px solid black" }}>
+        <h1>React Presentation Slides</h1>
+        <div contentEditable={true} style={{ width: "100%", height: "100%", backgroundColor: "white", border: "1px solid black" }}></div>
+      </div>
     </>
   )
 }
