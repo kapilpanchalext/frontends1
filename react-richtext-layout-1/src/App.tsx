a4c3932e8d8bf827ef169bec8822ad463346282e
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-07-22 17:46:19 +0530

    [react-richtext-layout-1] vertical scroll

---
+++ react-richtext-layout-1/src/App.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from "react";
+import './App.css';
+
+function App() {
+  const [percentage, setPercentage] = useState(50);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = Math.max(0, Math.min(100, Number(e.target.value)));
+    setPercentage(value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <div className="colored-div">
+        <h6 className="moving-line" style={{ top: `${percentage}%` }} >A4</h6>
+        <hr className="moving-line" style={{ top: `${percentage}%` }} />
+        <div className="color-fill" style={{ height: `${percentage}%` }}></div>
+      </div>
+      <input
+        type="number"
+        value={percentage}
+        onChange={handleInputChange}
+        min="0"
+        max="100"
+        style={{ marginTop: '20px' }}
+      />
+      <p>Color percentage: {percentage}%</p>
+    </div>
+  )
+}
+
+export default App;
