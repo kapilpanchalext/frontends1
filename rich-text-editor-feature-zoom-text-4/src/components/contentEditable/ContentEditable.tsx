456be2fa1c23053dfae2d1db6c8a9419930e691e
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-07-25 15:50:20 +0530

    [feature] added css for overflow

---
+++ rich-text-editor-feature-zoom-text-4/src/components/contentEditable/ContentEditable.tsx
@@ -0,0 +1,112 @@
+import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react';
+import "./ContentEditable.module.css";
+
+type Props = {
+  isReadonly: boolean;
+  onPaste: (isEditable: boolean) => void;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export interface ForwardRichTextData {
+  getRichTextRefData: () => HTMLDivElement | null;
+  getScrollHeight: () => number;
+  getScrollTop: () => number;
+  getClientHeight: () => number;
+}
+
+const ContentEditable = forwardRef<ForwardRichTextData, Props>(({onPaste, isReadonly, ...props}: Props, ref) => {
+  const contentEditableRef = useRef<HTMLDivElement>(null);
+
+  useImperativeHandle(ref, () => ({
+    getRichTextRefData() {
+      return contentEditableRef.current;
+    },
+    getScrollHeight() {
+      return contentEditableRef.current?.scrollHeight || 0;
+    },
+    getScrollTop(){
+      return contentEditableRef.current?.scrollTop || 0;
+    },
+    getClientHeight(){
+      return contentEditableRef.current?.clientHeight || 0;
+    }
+  }),[]);
+
+  useEffect(() => {
+    const editableDiv = contentEditableRef.current;
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Enter') {
+        if (editableDiv) {
+          onPaste(true);
+        }
+      }
+    };
+
+    const handleInput = () => {
+      onPaste(true);
+    };
+
+    const handlePaste = (event: ClipboardEvent) => {
+      if (editableDiv) {
+        event.preventDefault();
+        const html = event.clipboardData?.getData('text/html') || '';
+        const text = event.clipboardData?.getData('text/plain') || '';
+
+        // Fallback to plain text if HTML is not available
+        const content = html || text;
+
+        // Insert the content at the current cursor position
+        const selection = window.getSelection();
+        if (!selection?.rangeCount) {
+          return;
+        }
+
+        const range = selection.getRangeAt(0);
+        range.deleteContents();
+
+        const fragment = range.createContextualFragment(content);
+        range.insertNode(fragment);
+
+        // Move the cursor to the end of the inserted content
+        range.collapse(false);
+        selection.removeAllRanges();
+        selection.addRange(range);
+        onPaste(true);
+      }
+    };
+
+    if (editableDiv) {
+      editableDiv.addEventListener('keydown', handleKeyDown);
+      editableDiv.addEventListener('input', handleInput);
+      editableDiv.addEventListener('paste', handlePaste);
+    }
+
+    return () => {
+      if (editableDiv) {
+        editableDiv.removeEventListener('keydown', handleKeyDown);
+        editableDiv.removeEventListener('input', handleInput);
+        editableDiv.removeEventListener('paste', handlePaste);
+      }
+    };
+  }, [onPaste]);
+
+  return (
+    <div
+      {...props}
+      ref={contentEditableRef}
+      contentEditable={isReadonly}
+      style={{
+                border: '1px solid black', 
+                backgroundColor:"white", 
+                padding: '10px', 
+                width: '100%',
+                borderRadius: '10px', 
+                overflow: "auto",
+                zoom: 1,
+            }}
+            >
+    </div>
+  );
+});
+
+export default ContentEditable;
---
+++ rich-text-editor-feature-zoom-text-4/src/components/richTextLayout/RichTextLayout.tsx
@@ -0,0 +1,88 @@
+import { useState, useRef, useEffect, useMemo } from "react";
+import ButtonControls from "../buttonControls/ButtonControls";
+import ContentEditable, { ForwardRichTextData } from "../contentEditable/ContentEditable";
+import "./RichTextModule.css";
+
+type Props = {
+    layoutHeight: number;
+}
+
+const RichTextLayout = ({layoutHeight}: Props) => {
+  const [data, setData] = useState<string>(''); //Data to be sent to backend
+  const [isReadonly, setIsReadonly] = useState<boolean>(true);
+  const richTextData = useRef<ForwardRichTextData>(null);
+  const [isContentEditableEvent, setIsContentEditableEvent] = useState<boolean>(false);
+  const [scrollProgress, setScrollProgress] = useState<number>(0);
+  const pageMarkerRef = useRef<HTMLDivElement>(null);
+
+  const MAX_NUMBER_OF_PAGES = 1000;
+  const a4HeightPx = useMemo(() => (297 / 25.4) * 96, []);
+  const a4Heights = useMemo(() => Array.from({ length: MAX_NUMBER_OF_PAGES }, (_, index) => index * a4HeightPx), [a4HeightPx]);
+  
+  const handleScroll = () => {
+    const element1 = richTextData.current;
+    const element2 = pageMarkerRef.current;
+    const winScroll = element1!.getScrollTop();
+    const height = element1!.getScrollHeight() - element1!.getClientHeight();
+    const scrolled = (winScroll / height) * 100;
+
+    if (element1 && element2) {
+      const winScroll = element1!.getScrollTop();
+      setScrollProgress(scrolled);
+      element2.scrollTop = winScroll;
+    }
+  };
+
+  const onPasteHandler = (isEditable: boolean) => {
+    setIsContentEditableEvent(isEditable);
+  };
+
+  useEffect(() => {
+    if (richTextData.current) {
+      setData(richTextData.current.getRichTextRefData()?.innerHTML || '');
+      setIsContentEditableEvent(false);
+    }
+  }, [isContentEditableEvent]);
+
+  return (
+    <>
+      <div className="flex-container-row editor-content border-visible" style={{ height: `${layoutHeight}px` }}>
+        <div className="flex-container-column editor-content border-visible flex-item-1"
+          style={{ minWidth: "10%", marginRight: "10px", marginBottom: "10px" }}></div>
+
+        <div className="flex-container-column editor-content flex-item-8" style={{ marginLeft: "5px", padding: "5px" }}>
+          <div className="flex-container-row editor-content border-visible" style={{ marginLeft: "5px" }}>
+            <ButtonControls isReadonly={isReadonly} setIsReadonly={setIsReadonly}/>
+          </div>
+
+          <div style={{ display: "flex", flexDirection: "row", height: "2px", background: "#ccc", marginLeft: "15px", marginRight: "15px" }}>
+            <div style={{ height: "2px", background: "#04AA6D", width: `${scrollProgress}%` }}></div>
+          </div>
+
+          <div className="flex-container-row editor-content" style={{ marginLeft: "5px", height: "100%" }}>
+            <ContentEditable ref={richTextData} onScroll={handleScroll} onPaste={() => onPasteHandler(false)} isReadonly={isReadonly} />
+            <div
+              ref={pageMarkerRef}
+              className="flex-container-column editor-content border-visible"
+              style={{ marginLeft: "5px", width: "20px", position: "relative", overflow: "hidden" }}>
+              {a4Heights.map((height, index) => (
+                <div
+                  key={index}
+                  style={{
+                    position: "absolute",
+                    top: `${height}px`,
+                    width: "100%",
+                  }}>
+                  <hr className="moving-line" style={{ width: "100%" }} />
+                  <h6 className="moving-line" style={{ marginTop: "1px" }}>A4</h6>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default RichTextLayout;
