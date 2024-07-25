41bb44eff4f5875fca07ea1f9338dba12b3d7e14
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-07-25 14:51:31 +0530

    [rich-text-editor-feature-overflow-gui]

---
+++ rich-text-editor-feature-overflow-gui-2/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div contentEditable style={{ border: '1px solid black', height: '500px', overflow: 'auto' }}>
+
+      </div>
+    </>
+  )
+}
+
+export default App;
